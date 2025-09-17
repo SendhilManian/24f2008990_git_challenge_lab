31a2d95888e8c8c36c528ee977d2588215795d15
Sendhil Manian Vellaichamy <v.sendhilmanian@gmail.com>
2025-09-17 17:34:27 +0530

    Committing all files

---
+++ diff.py
@@ -0,0 +1,12 @@
+# Program to find the difference between two numbers
+
+# Input from user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the difference
+difference = num1 - num2
+
+# Display the result
+print("The difference is:", difference)
+
